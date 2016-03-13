0e95476984c811f481602e0de3bcc4e1fcd04c2a
vmcloudsolution <vmcloudsolution@gmail.com>
2016-03-13 13:33:21 -0500

    Carga inicial de modulo

---
+++ pos_validate_refund/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+##############################################################################
+#    Module : POS para Peru
+#    Personalizacion de POS para Peru
+#    Author : Elmer Ventocilla
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+##############################################################################
+import pos_order
