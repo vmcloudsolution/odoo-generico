1ebc567b8dc4a4a64bafdd0557cf3099a8ed90bc
ElmerVentocilla <eventocilla@vmcloudsolution.pe>
2017-01-18 23:22:40 -0500

    Se limpia para V10

--- pos_gift_voucher/static/src/js/main.js
+++
@@ -1,7 +0,0 @@
-openerp.pos_gift_voucher = function(instance){
-
-    var module = instance.point_of_sale;
-
-    openerp_pos_gift_voucher_screens(instance,module);
-    openerp_pos_gift_voucher_models(instance,module);
-};
--- posbox_proxy_backend/static/src/js/main.js
+++
@@ -1,8 +0,0 @@
-openerp.posbox_proxy_backend = function(instance) {
-
-    var module = instance.posbox_proxy_backend;
-
-    openerp_proxy_device(instance,module);
-};
-
-
